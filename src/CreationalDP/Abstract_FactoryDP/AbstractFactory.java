3f4357d3a4f239ca8bd89ee27f9a52444d5538b5
Ebubekir <ebubekirc442@gmail.com>
2023-06-20 09:41:09 +0300

    DesignPatten

--- src/CreationalDP/Abstract_FactoryDP/AbstractFactory.java
+++
@@ -1,9 +0,0 @@
-package CreationalDP.Abstract_FactoryDP;
-
-public  abstract class AbstractFactory {
-
-    abstract Color getColor(String color );
-    abstract Shape getShape(String shape);
-
-
-}
--- src/CreationalDP/Abstract_FactoryDP/Green.java
+++
@@ -1,8 +0,0 @@
-package CreationalDP.Abstract_FactoryDP;
-
-public class Green implements Color{
-    @Override
-    public void fil() {
-        System.out.println("yesile boyandi");
-    }
-}
